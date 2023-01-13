087d2eb5fc6e7747cc4835cc052cc7f7ab4fc4a0
Payton <30909416+Trosclair@users.noreply.github.com>
2023-01-13 03:36:08 -0600

    Add a couple new Converters

---
+++ WPF Utilities/Converters/BoolTo/BoolToVisibility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF_Utilities.Converters.BoolTo
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        public Visibility TrueVisibility { get; set; } = Visibility.Visible;
+        public Visibility FalseVisibility { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is bool boolean && boolean) ? TrueVisibility : FalseVisibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is Visibility visibility && visibility == TrueVisibility;
+        }
+    }
+}
---
+++ WPF Utilities/Converters/NullTo/NullToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Data;
+using System.Windows;
+using System;
+
+namespace WPF_Utilities.Converters.NullTo
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public Visibility NotNullVisibility { get; set; } = Visibility.Visible;
+        public Visibility NullVisibility { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is not null) ? NotNullVisibility : NullVisibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
