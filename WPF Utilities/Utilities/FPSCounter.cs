ea1f19cc4aaae000172393ee4e786d30761c790f
Payton <30909416+Trosclair@users.noreply.github.com>
2023-01-13 16:39:45 -0600

    Added FPS counter to utilities

---
+++ WPF Utilities/Utilities/FPSCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace WPFUtilities.Utilities
+{
+    public class FPSCounter : ObservableObject
+    {
+        private static long frameRateTime = 0;
+        private static int frameRate = 0;
+        private static int frameCount = 0;
+
+        private static Stopwatch globalTimer = new();
+
+        public int FrameRate { get => frameRate; set { frameRate = value; OnPropertyChanged(nameof(FrameRate)); } }
+
+        public FPSCounter() { globalTimer.Start(); }
+
+        /// <summary>
+        /// Add this to the main processing loop.
+        /// </summary>
+        public void UpdateFPS()
+        {
+            if (globalTimer.ElapsedMilliseconds > frameRateTime + 1000)
+            {
+                FrameRate = frameCount;
+                frameCount = 0;
+                frameRateTime = globalTimer.ElapsedMilliseconds;
+            }
+            frameCount++;
+        }
+    }
+}
