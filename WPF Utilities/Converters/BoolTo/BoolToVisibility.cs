580f3f3d30ea6e7e4dcdf0c801ebc6809f07848b
Payton <30909416+Trosclair@users.noreply.github.com>
2023-01-13 03:36:08 -0600

    Add a couple new Converters

---
+++ WPF Utilities/Converters/BoolTo/BoolToVisibility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace WPF_Utilities.Converters.BoolTo
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        public Visibility TrueVisibility { get; set; } = Visibility.Visible;
+        public Visibility FalseVisibility { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value is bool boolean && boolean) ? TrueVisibility : FalseVisibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value is Visibility visibility && visibility == TrueVisibility;
+        }
+    }
+}
